e4910db87c5e1269456b6071d6a422884552107f
zTags <weirdgeomery@gmail.com>
2022-05-21 13:03:24 +0200

    setup

---
+++ LuaEngine.Backend/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LuaEngine.Backend
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
